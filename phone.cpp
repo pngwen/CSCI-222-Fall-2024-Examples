407a4c30e61337ecfe45bf1124ebd91543258ee6
Robert Lowe <pngwen@gmail.com>
2024-09-06 14:51:36 +0000

    phone example

--- phone.cpp
+++ phone.cpp
@@ -4,6 +4,69 @@
 
 using namespace std;
 
+const int MAX_PHONE=20;
+
+
+/**
+ * Validate that the given phone number is ###-###-####
+ */
+bool isValidPhone(char phone[]);
+
+bool isDigit(char c);
+
 int main() 
 {
+    char phone[MAX_PHONE];
+
+    // Note: The following is very dangerous
+    cout << "Enter a phone number: ";
+    cin >> phone;
+
+    // print the phone number
+    cout << phone << " is ";
+    if(isValidPhone(phone)) {
+        cout << "valid";
+    } else {
+        cout << "invalid";
+    }
+    cout << endl;
+}
+
+
+bool isValidPhone(char phone[]) {
+    // validate 3 digits
+    for(int i=0; i<3; i++) {
+        if(not isDigit(phone[i])) {
+            return false;
+        }
+    }
+
+    // validate dash
+    if(phone[3] != '-') {
+        return false;
+    }
+
+    // validate 3 digits
+    for(int i=4; i<7; i++) {
+        if(not isDigit(phone[i])) {
+            return false;
+        }
+    }
+
+    // validate dash
+    if(phone[7] != '-') {
+        return false;
+    }
+
+    for(int i=8; i<12; i++) {
+        if(not isDigit(phone[i])) {
+            return false;
+        }
+    }
+
+    return phone[12] == 0;
+}
+
+bool isDigit(char c) {
+    return c >= '0' and c <= '9';
 }
